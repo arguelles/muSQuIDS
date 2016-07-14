56f46bb780966bf5041704d0b76b4d0b119ffaaa
Carlos Arguelles <arguelles@wisc.edu>
2016-07-13 19:35:20 -0500

    it compiles - for real

--- mains/run_musquids.cpp
+++ mains/run_musquids.cpp
@@ -12,8 +12,8 @@ int main()
 {
   squids::Const units;
 
-  auto e_nodes = logspace(1.0e1*units.GeV,1.0e3*units.GeV,100);
-  //muSQUIDS(e_nodes);
+  nusquids::marray<double,1> e_nodes = logspace(1.0e1*units.GeV,1.0e3*units.GeV,100);
+  muSQUIDS musq(e_nodes);
 
   return 0;
 }
